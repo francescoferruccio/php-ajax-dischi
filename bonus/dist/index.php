093835d75d29b69ad1508602e098cc18b6fc5570
Francesco <francesco.ferruccio88@gmail.com>
2020-05-09 14:56:47 +0200

    aggiunto hover btn search

--- bonus/dist/index.php
+++ bonus/dist/index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>PHP Dischi</title>
+    <title>PHP Dischi BONUS</title>
     <link rel="stylesheet" href="app.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
